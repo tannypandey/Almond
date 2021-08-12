2b637c26b48f6051b323b6ac84ae83446cc96ded
Tanvi Pandey <61287317+tannypandey@users.noreply.github.com>
2021-08-12 15:34:45 +0100

    Add files via upload

---
+++ Telegram/index.js
@@ -0,0 +1,180 @@
+// This is not the final code
+// Authorization code is still left
+// Sending and receiving messages is pretty much done but some tweaks are left
+
+
+const Tp = require('thingpedia');
+const Url = require('url');
+
+const telegram;// = require('telegram-node-client').Telegram;
+const FormData = require('form-data');
+
+// encryption
+function rot13(x) {
+    return Array.prototype.map.call(x, (ch) => {
+        var code = ch.charCodeAt(0);
+        if (code >= 0x41 && code <= 0x5a)
+            code = (((code - 0x41) + 13) % 26) + 0x41;
+        else if (code >= 0x61 && code <= 0x7a)
+            code = (((code - 0x61) + 13) % 26) + 0x61;
+
+        return String.fromCharCode(code);
+    }).join('');
+}
+
+const CONSUMER_KEY = process.env['telegram_CONSUMER_KEY'] || 'VZRViA2T4qy7CBZjU5juPumZN';
+// telegram uses OAuth 1.0, so this needs to be here...
+const CONSUMER_SECRET = process.env['telegram_CONSUMER_SECRET'] || rot13('hsTCqM6neIt3hqum6zvnDCIqQkUuyWtSjKBoqZFONvzVXfb7OJ');
+
+function makeTelegramApi(engine, accessToken, accessTokenSecret) {
+    var origin = engine.platform.getOrigin();
+    return new Telegram({
+        consumerKey: CONSUMER_KEY,
+        consumerSecret: CONSUMER_SECRET,
+        callBackUrl: origin + '/devices/oauth2/callback/com.telegram',
+        accessToken: accessToken,
+        accessTokenSecret: accessTokenSecret
+    });
+}
+
+function runOAuthStep1(engine) {
+    let telegram = makeTelegramApi(engine);
+
+    return new Promise((callback, errback) => {
+        telegram.oauth.getOAuthRequestToken((error, oauth_token, oauth_token_secret, query) => {
+            if (error)
+                errback(error);
+            else
+                callback({ token: oauth_token, tokenSecret: oauth_token_secret, query: query });
+        });
+    }).then((result) => {
+        const url = Url.parse('https://api.telegram.com/oauth/authorize');
+        url.query = result.query;
+        url.query['oauth_token'] = result.token;
+        url.query['oauth_token_secret'] = result.tokenSecret;
+        return [Url.format(url), { 'telegram-token': result.token,
+                                   'telegram-token-secret': result.tokenSecret }];
+    });
+}
+
+function runOAuthStep2(engine, req) {
+    let telegram = makeTelegramApi(engine);
+
+    return new Promise((callback, errback) => {
+        const token = req.session['telegram-token'];
+        const tokenSecret = req.session['telegram-token-secret'];
+        const verifier = req.query['oauth_verifier'];
+
+        telegram.oauth.getOAuthAccessToken(token, tokenSecret, verifier, (error, oauth_access_token, oauth_access_token_secret, results) => {
+            if (error)
+                errback(error);
+            else
+                callback({ accessToken: oauth_access_token, accessTokenSecret: oauth_access_token_secret });
+        });
+    }).then((result) => {
+        telegram = maketelegramApi(engine, result.accessToken, result.accessTokenSecret);
+        return new Promise((callback, errback) => {
+            telegram.getCustomApiCall('/account/verify_credentials.json', {}, errback, callback);
+        });
+    }).then((result) => {
+        result = JSON.parse(result);
+        return engine.devices.loadOneDevice({ kind: 'com.telegram',
+                                              accessToken: telegram.accessToken,
+                                              accessTokenSecret: telegram.accessTokenSecret,
+                                              userId: result['id_str'],
+                                              screenName: result['screen_name'] }, true);
+    });
+}
+
+// a fixed version of postCustomApiCall that does not append
+// the parameters to the url (which would break OAuth)
+function postCustomApiCall(url, params, error, success) {
+    url = this.baseUrl + url;
+    this.doPost(url, params, error, success);
+}
+
+
+module.exports = class TelegramAccountDevice extends Tp.BaseDevice {
+    static runOAuth2(engine, req) {
+        return Promise.resolve().then(() => {
+            if (req === null)
+                return runOAuthStep1(engine);
+            else
+                return runOAuthStep2(engine, req);
+        }).catch((e) => {
+            console.log(e);
+            console.log(e.stack);
+            throw e;
+        });
+    }
+
+    constructor(engine, state) {
+         super(engine, state);
+         // constructor
+    }
+
+    // other methods of device class
+
+    //for reading messages
+    _pollDirectMessages(since_id) {
+        return new Promise((callback, errback) => {
+            if (since_id !== undefined)
+                this._telegram.getCustomApiCall('/direct_messages/events/list.json', { since_id: since_id, count: 20 }, errback, callback);
+            else
+                this._telegram.getCustomApiCall('/direct_messages/events/list.json', { count: 20 }, errback, callback);
+        }).then((results) => Promise.all(JSON.parse(results).events.map((dm) => {
+            console.log(JSON.stringify(dm));
+            return this._pollUserScreenName(dm.message_create.sender_id).then((screen_name) => {
+                return {
+                    sender: screen_name,
+                    message: dm.message_create.message_data.text
+                };
+            });
+        }))).then((results) => results.filter((dm) => dm.sender.toLowerCase() !== this.screenName.toLowerCase()));
+    }
+
+    get_direct_messages(params, filters) {
+        return this._pollDirectMessages(undefined);
+    }
+
+    //for sending messages
+    do_send_direct_message({ to, message }) {
+        return new Promise((callback, errback) => {
+            this._pollUserId(String(to)).then((user_id) => {
+                if (!user_id)
+                    throw new Error('User not found');
+                const data = {
+                    event: {
+                        type: 'message_create',
+                        message_create: {
+                            target: { recipient_id: user_id },
+                            message_data: { text: String(message) }
+                        }
+                    }
+                };
+                this._telegram.oauth.post(this._telegram.baseUrl + '/direct_messages/events/new.json',
+                    this._telegram.accessToken,
+                    this._telegram.accessTokenSecret,
+                    JSON.stringify(data),
+                    "application/json",
+                    (err, body, response) => {
+                        if (!err && response.statusCode === 200)
+                            callback(body);
+                        else
+                            errback(err, response, body);
+                    });
+            });
+        }).catch((e) => {
+            if (e.message && (!e.data && !e.errors))
+                throw e;
+
+            console.error('Failed to send direct message', e);
+            if (e.data && e.data)
+                throw new Error(JSON.parse(e.data).errors[0].message);
+            else if (e.errors)
+                throw new Error(e.errors[0].message);
+            else
+                throw new Error(String(e));
+        });
+    }
+};
